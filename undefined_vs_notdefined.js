9cfe26575e19894e1af4ee9d851efb0a7f521eba
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-09-09 23:57:56 +0530

    Undefined vs Not Defined in JS

---
+++ undefined_vs_notdefined.js
@@ -0,0 +1,17 @@
+console.log(a);
+var a = 7;
+
+console.log(x);
+
+
+/* UNDEFINED: 
+- Undefined is a primitive data type in javascript.
+- It means that a variable is declared but not assigned a value.
+
+- Before the code is executed, variables and functions are allocated memory in the execution context. 
+- The variables are allocated the value undefined.
+- After a value is assigned to the variable, undefined changes to the value assigned.
+
+- So, the first console statement will print undefined.
+
+*/ 
