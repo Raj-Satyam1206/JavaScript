aab884c4172c56df1d68dd93f7058ed3ab7c55de
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-09-10 00:04:48 +0530

    Undefined vs Not Defined in JS

--- undefined_vs_notdefined.js
+++ undefined_vs_notdefined.js
@@ -12,6 +12,31 @@ console.log(x);
 - The variables are allocated the value undefined.
 - After a value is assigned to the variable, undefined changes to the value assigned.
 
+- Undefined is like a placeholder given to the variable which stays with it until a value is assigned to the varibale.
+
+- UNDEFINED TAKES MEMORY SPACE.
+
 - So, the first console statement will print undefined.
+
+
+
+    NOT DEFINED:
+- Not Defined means when the variable is used without first declaring the variable.('x')
+
+- In the execution context, memory is not allocate dto these variables. 
+*/ 
+
+
+
+var a;
+a=10;
+console.log(a);
+a="Hello"
+console.log(a);
+
+/*  JS is a dynamically-typed language.
+- It means that the data type of a variable is determined at runtime, not at compile time.
+- The data type of a variable can change during the execution of the program.
+
 
 */ 
