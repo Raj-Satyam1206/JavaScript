8ebe723cf0dfddef35edd40bcc683da06a99bc05
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-09-15 17:00:42 +0530

    Closures Modified

--- Closures.js
+++ Closures.js
@@ -61,3 +61,32 @@ WRONG.
 With the closure(function), the reference of 'a' is returned. 
 It remembers the reference of 'a', and when z() invokes, 100 is present in the memeory space of variable'a'. So, it prints 100.
 */
+
+
+//OUTPUT QUESTIONS
+
+function x(){
+    // var a=7;
+    function y(){
+        console.log(a); // REFERENCE of 'a'
+    }
+    var a = 100;
+    return y;
+}
+x()();
+
+// y() will still form a closure with variable a as 'a' is still in the outer lexical scope of y(). It does not matter in which sequence it is written.
+
+
+function x(){
+    // var a=7;
+    function y(){
+        console.log(a); // REFERENCE of 'a'
+    }
+    // var a = 100;
+    let b = 10;
+    return y;
+}
+x()();
+
+// y() will still form a closure with variable 'b' even if it is declared with LET keyword. let has a block scope.
