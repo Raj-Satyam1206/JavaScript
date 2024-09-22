4ce16bfe72a93d136592c170aaff4980de179865
Raj-Satyam1206 <raj.satyam1229@gmail.com>
2024-09-22 19:15:06 +0530

    JS Engine Explained

--- JS_Engine.js
+++ JS_Engine.js
@@ -13,6 +13,10 @@ A.  JRE is an environment in which the JS code is run.
 
     Browser has its own JRE to run the JS code and Node.js has its own JRE to run the JS code on the server.
 
+
+
+
+
 Q. What is a JS Engine?
 A. 
 JS Engine is the heart of JRE where the JS code is computed and executed.
@@ -23,11 +27,19 @@ Different JS Engines:
 First JS Engine was made by Brendan Eich as Spider Monkey, used in Mozilla Firefox today.
 
 
+
+
+
+
 Q. Three phases of JS Engine?
 A. 
 1. Parsing: JS Engine reads the high-level source code and breaks it into tokens.(Abstract Syntax Tree)
 2. Compilation: JS Engine converts the tokens(AST) into bytecode.
 3. Execution: JS Engine executes the bytecode.
+
+
+
+
 
 
 Q. What is the difference between Interpreter and Compiler?
@@ -36,6 +48,10 @@ A.
 Interpreter takes the high-level source code and starts executing line by line straight away. (MORE SPEED)
 
 Compiler compiles the entire source code before executing and then converts it into an intermediate optimized code and then executed.(MORE EFFICIENT)
+
+
+
+
 
 
 Q. Is JS an interpreted-language or compiled-language?
@@ -47,6 +63,9 @@ A.  JS is a JIT compiled Language(Just-In-Time).
     Interpreter converts the AST into bytecode , which is ready to be executed and the compiler optimizes the bytecode at RUNTIME.
 
 
+
+
+    
 
 Q. How JS Engine works?
 
